5e356c7b686a9b1742d27815a94c57afbef4f87f
Raktim <www.nghosal@gmail.com>
2023-03-12 20:10:23 +0530

    Added Description for JAVADOC

--- src/java/com/exavalu/services/TestMailService.java
+++ src/java/com/exavalu/services/TestMailService.java
@@ -7,28 +7,36 @@ package com.exavalu.services;
 import com.exavalu.utils.AuthUtility;
 import java.time.LocalDateTime;
 import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import org.apache.log4j.Logger;
 
 /**
- *  This service class integrates the sending gmail functionality from the application
+ * This service class integrates the sending gmail functionality from the
+ * application
+ *
  * @author Raktim Ghosal
  */
 public class TestMailService {
 
-    public static void send( String to, String subject, String text) {
-        
+    public static void send(String to, String subject, String textData) {
+
         AuthUtility authUtility = AuthUtility.getInstanceOfAuthUtility();
         //Get properties object    
-        final String from= authUtility.getPropertyValue("email");
+        final String from = authUtility.getPropertyValue("email");
         final String password = authUtility.getPropertyValue("password");
-        
+
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
@@ -48,17 +56,38 @@ public class TestMailService {
         try {
             MimeMessage message = new MimeMessage(session);
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            MimeMultipart multipart = new MimeMultipart("related");
+            
             message.setSubject(subject);
-            message.setText(text);
+            
+            MimeBodyPart messageBodyPart1 = new MimeBodyPart();
+            String htmlText = "<html><body><img src=\"cid:image\" width=\"100\" height=\"100\"><br><b>" + textData + "</b></body></html>";
+            messageBodyPart1.setContent(htmlText, "text/html");
+            // add it
+            multipart.addBodyPart(messageBodyPart1);
+
+            // second part (the image)
+            MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+            DataSource fds = new FileDataSource(
+                    "D:/DMS/DisasterMgmtSystem/web/image/mainlogo.jpg");
+
+            messageBodyPart2.setDataHandler(new DataHandler(fds));
+            messageBodyPart2.setHeader("Content-ID", "<image>");
+
+            // add image to the multipart
+            multipart.addBodyPart(messageBodyPart2);
+
+            // put everything together            
+            message.setContent(multipart);
             //send message  
             Transport.send(message);
             System.out.println("message sent successfully");
         } catch (MessagingException ex) {
             Logger log = Logger.getLogger(TestMailService.class.getName());
-            log.error(LocalDateTime.now()+ " Error Message: " + ex.getMessage()+" Class : TestMailService, Method : send");
-        }         
-            System.out.println(from+" "+to+" "+password);
+            log.error(LocalDateTime.now() + " Error Message: " + ex.getMessage() + " Class : TestMailService, Method : send");
+        }
+        System.out.println(from + " " + to + " " + password);
     }
-    
-}
 
+}
